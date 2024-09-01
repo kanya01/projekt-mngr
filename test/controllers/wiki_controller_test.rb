5c2a6242cc3acfc4d86762dee1402934d5fb94c8
kanya01 <kanyamosesm@gmail.com>
2024-09-01 11:49:13 +0100

    ---
    ---

---
+++ test/controllers/wiki_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class WikiControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get wiki_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get wiki_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get wiki_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get wiki_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get wiki_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get wiki_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get wiki_destroy_url
+    assert_response :success
+  end
+end
